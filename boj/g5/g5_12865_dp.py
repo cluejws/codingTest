76aace72d7662fb7b6b6d87909923ff78772ade6
WonSeok-dd <cluejws@gmail.com>
2023-01-16 22:08:53 +0900

    update boj_g5_12685 평범한 배낭 - dp - dp[i][k]: 보석 i까지 고려했을 때의 / k무게 만큼 담을때 / 최대 가치

--- boj/g5/g5_12865_dp.py
+++ boj/g5/g5_12865_dp.py
@@ -12,24 +12,16 @@ for _ in range(n):
 # i: 0번째 부터 i번째까지 배낭 고려
 # j: 무게
 # dp[i][k]: i까지 고려했을때의 k무게 만큼 담고 후의 최대가치들
-dp = [[-1 for _ in range(k+1)] for _ in range(n)]
-dp[0][0] = 0
-if weight[0] <= k:
-    dp[0][weight[0]] = value[0]
+dp = [[0 for _ in range(k+1)] for _ in range(n)]
 
 # 계산2: DP 구하기
-for i in range(1,n):
+for i in range(n):
     for j in range(k+1):
 
-        if j >= weight[i] and dp[i-1][j - weight[i]] != -1:
+        if j >= weight[i]:
             dp[i][j] = max(dp[i-1][j - weight[i]] + value[i], dp[i-1][j])
         else:
             dp[i][j] = dp[i-1][j]
 
-# 최대값 출력
-max_num = 0
-for i in range(len(dp)):
-    max_dp = max(dp[i])
-    if max_dp > max_num:
-        max_num = max_dp
-print(max_num)
+# 출력
+print(dp[n-1][k])
