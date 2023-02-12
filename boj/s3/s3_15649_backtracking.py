3d90ebb8b655849b37b48348abaf11d59e4a3a2b
WonSeok-dd <cluejws@gmail.com>
2023-02-13 02:25:47 +0900

    update boj_s3_15649 N과 M (1) - 백트래킹

---
+++ boj/s3/s3_15649_backtracking.py
@@ -0,0 +1,23 @@
+def getResult():
+    
+    if len(arr) == m:
+        print(*arr)
+        return
+    
+    for i in range(1, n+1):
+        if i in arr:
+            continue
+        else:
+            arr.append(i)
+            getResult()
+            arr.pop()
+    
+# 입력
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+
+# 계산
+arr = []
+getResult()
